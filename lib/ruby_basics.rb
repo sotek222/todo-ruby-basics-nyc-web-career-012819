e9d71386f78b5041acfcb66e2487d08947b5f645
Matthew Masiello <home@Matthews-MBP.fios-router.home>
2019-01-12 19:22:03 -0500

    Done.

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -1,27 +1,28 @@
 def division(num1, num2)
- 
+  num1/num2
 end
 
 def assign_variable(value)
-  
+  name = value
+  name
 end
 
-def argue
-  
+def argue x
+  x
 end
 
-def greeting
-
-end
-
-def return_a_value
+def greeting(greeting, name)
 
 end
 
-def last_evaluated_value
-
+def return_a_value phrase = "Nice"
+  phrase
 end
 
-def pizza_party
+def last_evaluated_value phrase = "expert"
+  phrase
+end
 
+def pizza_party topping = "cheese"
+  topping
 end
